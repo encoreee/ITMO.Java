082b29e7b9bcc597c9c9ea81e8c9ca5bb7987803
auroraclub <alexraykov2000@gmail.com>
2021-09-27 22:49:01 +0300

    final start

---
+++ FinalTask/src/main/java/Bot.java
@@ -0,0 +1,48 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotUsername() {
+        return "weatheralexraykov_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "2014200237:AAF93pWFmiLaglyFPO2fM410inS24znVWjY";
+    }
+
+    @Override
+    public void onRegister() {
+        super.onRegister();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();// Create a message object object
+            message.setChatId(Long.toString(chat_id));
+            message.setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+}
